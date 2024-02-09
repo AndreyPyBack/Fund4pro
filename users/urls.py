972112d4ef2b96194132adc8479346c7b23c204c
zarimas7 <zarimas28@gmail.com>
2024-02-09 16:13:17 +0600

    two

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import UserLoginView, InterestSectorListCreateAPIView, InvestorCreateAPIView, BusinessmanCreateAPIView, \
-    SendEmailVerification
+from .views import UserLoginView, InterestSectorListCreateAPIView, InvestorCreateAPIView, BusinessmanCreateAPIView
 
 urlpatterns = [
     path('api/interest-sectors/', InterestSectorListCreateAPIView.as_view(), name='interest-sectors'),
     path('api/register/investor/', InvestorCreateAPIView.as_view(), name='register-investor'),
     path('api/register/businessman/', BusinessmanCreateAPIView.as_view(), name='businessman-register'),
     path('api/login/', UserLoginView.as_view(), name='users-login'),
-    path('api/confirm_email/', SendEmailVerification.as_view(), name='user-confirm'),
+
 ]
